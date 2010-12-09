7096f10dbaddb6bc71514d94a7f691a3adb313fe
Chris Howse <chowse@mozilla.com>
2010-12-09 09:53:36 -0800

    Added visual feedback on form submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 
 
 (function() {
+	document.addEventListener('submit', onSubmit, false);
+
+	
+	var submitOK = false;
+	
+	function onSubmit(e) {
+		var f = e.target;
+		
+		if (submitOK) {
+			alert('submitOK');
+			submitOK = false;
+			return;
+		}
+		
+		var b = f.getElementsByTagName('button')[0];
+		b.disabled = true;
+		setTimeout(function() {
+			b.disabled = false;
+			submitOK = true;
+			f.submit();
+		}, 1000);
+		
+		e.preventDefault();
+		return false;
+	}
 })();
